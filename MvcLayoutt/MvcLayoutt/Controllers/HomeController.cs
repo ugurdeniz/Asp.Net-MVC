f4f303fa508f3edc73c4dfd1952ce71c484397f7
Uğur Deniz <ugur2353@gmail.com>
2017-08-18 13:39:17 +0300

    MVCLayoutExample

---
+++ MvcLayoutt/MvcLayoutt/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcLayoutt.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Iletisim()
+        {
+
+            return View();
+        }
+        public ActionResult Referanslar()
+        {
+
+            return View();
+        }
+        public ActionResult AdaKonut()
+        {
+            return View();
+        }
+        public ActionResult GuzelBahceKonut()
+        {
+            return View();
+        }
+        public ActionResult MerkezYasamKonut()
+        {
+            return View();
+        }
+    }
+}
